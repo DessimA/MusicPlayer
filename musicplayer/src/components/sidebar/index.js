2b7029f0477c45c49024d465cd2eebdfef9ab060
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2023-02-01 20:58:20 -0300

    Clean and finish

--- musicplayer/src/components/sidebar/index.js
+++ musicplayer/src/components/sidebar/index.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./sidebar.css";
 import SidebarButton from "./sidebarButton";
-import { MdOutlineFeed, MdFavorite } from "react-icons/md";
-import { FaGripfire, FaPlay, FaSignOutAlt } from "react-icons/fa";
+import {  FaPlay, FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import apiClient from "../../spotify"
 
@@ -20,15 +19,8 @@ useEffect(() =>{
   return (
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
-      <div>
-        <SidebarButton title="Feed" to="/feed" icon={<MdOutlineFeed />} />
-        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
-        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
-        <SidebarButton
-          title="Favorites"
-          to="/favorites"
-          icon={<MdFavorite />}
-        />
+      <div>        
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />       
         <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
       <SidebarButton title="Sing Out" to="" icon={<FaSignOutAlt />} />
--- musicplayer/src/screens/home/index.js
+++ musicplayer/src/screens/home/index.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
-import Favorites from "../favorites/favorites";
-import Feed from "../feed/feed";
 import Library from "../library";
 import Player from "../player/player";
-import Trending from "../trending/trending";
 import Login from "../auth/login";
 import "./home.css";
 import { setClientToken } from "../../spotify";
@@ -36,10 +33,7 @@ export default function Home() {
         <Sidebar />
         <Routes>
           <Route path="/" element={<Library />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/trending" element={<Trending />} />
           <Route path="/player" element={<Player />} />
-          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </Router>
     </div>
